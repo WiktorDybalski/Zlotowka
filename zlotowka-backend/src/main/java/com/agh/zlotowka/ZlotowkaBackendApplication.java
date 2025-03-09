4248220d2131e4ce0d1d0c7ead8b79b67a0322e1
Kamil Rudny <k.rudny1@gmail.com>
2025-03-09 19:29:43 +0100

    Backend setup

---
+++ zlotowka-backend/src/main/java/com/agh/zlotowka/ZlotowkaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.agh.zlotowka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZlotowkaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZlotowkaBackendApplication.class, args);
+	}
+
+}
---
+++ zlotowka-backend/src/test/java/com/agh/zlotowka/ZlotowkaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agh.zlotowka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZlotowkaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
