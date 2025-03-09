7a82e72c571289efa9437a352ac95c83774d4647
Kamil Rudny <k.rudny1@gmail.com>
2025-03-09 19:39:52 +0100

    Test endpoint

--- zlotowka-frontend/app/page.tsx
+++ zlotowka-frontend/app/page.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
+export default async function Home() {
+  const data = await fetch("http://localhost:8080/test")
+      .then((response) => response.text())
+      .catch((error) => console.error(error));
+
   return (
-      <div className="w-screen h-screen flex justify-center items-center">
-        <h1 className="text-4xl -mt-36">Hello World</h1>
+      <div className="w-screen h-screen flex flex-col justify-center items-center">
+        <h1 className="text-4xl -mt-36 mb-4">Hello World</h1>
+        <h2>{data ? data : "Loading..."}</h2>
       </div>
 
   );
