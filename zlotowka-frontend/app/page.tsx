1ccb9fc497212f6c7ba71c5a00f25f5195731c02
Kamil Rudny <k.rudny1@gmail.com>
2025-03-09 19:27:05 +0100

    Frontend setup

---
+++ zlotowka-frontend/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+      <div className="w-screen h-screen flex justify-center items-center">
+        <h1 className="text-4xl -mt-36">Hello World</h1>
+      </div>
+
+  );
+}
