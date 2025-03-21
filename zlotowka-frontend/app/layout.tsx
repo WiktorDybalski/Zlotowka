a9fa664af6604bca7d4c01c9492419b1252fe82c
Kamil Rudny <k.rudny1@gmail.com>
2025-03-22 00:23:00 +0100

    Adding transaction popup

--- zlotowka-frontend/app/layout.tsx
+++ zlotowka-frontend/app/layout.tsx
@@ -11,6 +11,8 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link rel="stylesheet"
+              href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=add"/>
         <link
             href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=optional&family=Raleway:ital,wght@0,100..900;1,100..900&display=optional"
             rel="stylesheet"/>
--- zlotowka-frontend/app/page.tsx
+++ zlotowka-frontend/app/page.tsx
@@ -1,13 +1,35 @@
-export default async function Home() {
-  const data = await fetch("https://zlotowka-621092586366.us-central1.run.app/test")
-      .then((response) => response.text())
-      .catch((error) => console.error(error));
+"use client"
+
+import { useEffect, useState } from "react";
+import AddTransaction from "@/components/AddTransaction";
+
+export default function Home() {
+  const [data, setData] = useState<string | null>(null);
+  const [showAddTransaction, setShowAddTransaction] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://zlotowka-621092586366.us-central1.run.app/test");
+        const text = await response.text();
+        setData(text);
+      } catch (error) {
+        console.error("Błąd podczas pobierania danych:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const toggle = () => {
+    setShowAddTransaction(!showAddTransaction);
+  };
 
   return (
       <div className="h-[200vh] flex flex-col justify-center items-center">
         <h1 className="text-4xl -mt-36 mb-4">Scrollable Hello World</h1>
         <h2>{data ? data : "Loading..."}</h2>
+        <button className="border-neutral-900 border-2 px-2 py-3 mt-4" onClick={toggle}>Add Transaction</button>
+        {showAddTransaction && <AddTransaction setShowAddTransaction={setShowAddTransaction} />}
       </div>
-
   );
 }
