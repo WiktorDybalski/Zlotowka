611c68d37cab10e438e282bdaed8a5bc9588cf8b
Kamil Rudny <k.rudny1@gmail.com>
2025-03-25 17:36:09 +0100

    Minor change

--- zlotowka-frontend/components/navigation/Navigation.tsx
+++ zlotowka-frontend/components/navigation/Navigation.tsx
@@ -10,7 +10,7 @@ export default function Navigation() {
   const [isOpen, setIsOpen] = useState(false);
   const navLinks: string[] = ["Dashboard", "Transakcje", "Marzenia"];
   const baseClasses = "w-full flex flex-col justify-center px-8 bg-[#262626] transition-all duration-300 rounded-b-lg";
-  const mobileClasses = isOpen ? "h-screen pb-6 absolute top-0 left-0" : "h-20";
+  const mobileClasses = isOpen ? "h-[100dvh] pb-6 absolute top-0 left-0" : "h-20";
   const desktopClasses = "lg:h-screen lg:py-4 lg:pb-8 lg:rounded-r-lg lg:rounded-bl-none";
 
   useEffect(() => {
