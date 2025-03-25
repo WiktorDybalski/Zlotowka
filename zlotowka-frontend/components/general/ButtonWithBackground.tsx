d8bdc3ad98544a5841cc13b2101a818005df8c5d
Bartłomiej Szubiak <bszubiak@student.agh.edu.pl>
2025-03-25 22:18:47 +0100

    #landing page, new Next, Schadcn added without theirs global.css

---
+++ zlotowka-frontend/components/general/ButtonWithBackground.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import clsx from "clsx";
+
+interface RegisterButtonProps {
+  onClick?: () => void;
+  className?: string;
+  children?: React.ReactNode;
+}
+
+export default function RegisterButton({
+  onClick,
+  className,
+  children,
+}: RegisterButtonProps) {
+  return (
+    <Button
+      className={clsx(
+        "bg-accent hover:bg-backgroundDark text-background px-8 py-6 text-lg rounded-md",
+        className
+      )}
+      onClick={onClick}
+    >
+      {children}
+    </Button>
+  );
+}
