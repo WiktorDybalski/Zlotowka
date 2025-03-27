773659fe84fe106173c022ca347879a6fc5f68d0
Wiktor Dybalski <wdybalski@student.agh.edu.pl>
2025-03-27 01:17:32 +0100

    Added database models

---
+++ zlotowka-backend/src/main/java/com/agh/zlotowka/model/RecurringTransaction.java
@@ -0,0 +1,63 @@
+package com.agh.zlotowka.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "recurring_transactions")
+public class RecurringTransaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "transaction_id")
+    private Integer transactionId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    private User user;
+
+    @Column(name = "name", length = 64)
+    private String name;
+
+    @Column(name = "amount")
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "currency_id", referencedColumnName = "currency_id")
+    private Currency currency;
+
+    @Column(name = "income")
+    private Boolean income;
+
+    @Column(name = "start_payment_date")
+    private LocalDate startPaymentDate;
+
+    @Column(name = "end_payment_date")
+    private LocalDate endPaymentDate;
+
+    @Column(name = "interval_col", nullable = false)
+    private Duration interval;
+
+    @Column(name = "last_payment_date")
+    private LocalDate lastPaymentDate;
+
+    @Column(name = "description", length = 512)
+    private String description;
+}
