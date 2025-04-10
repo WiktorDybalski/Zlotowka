c2e5fcbdfdd4e16cdbab2c360585981a9ccd5781
Kamil Rudny <k.rudny1@gmail.com>
2025-04-10 22:53:29 +0200

    Temp web config fix

--- zlotowka-backend/src/main/java/com/agh/zlotowka/security/WebConfig.java
+++ zlotowka-backend/src/main/java/com/agh/zlotowka/security/WebConfig.java
@@ -4,15 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//TODO: origins!
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://zlotowka.vercel.app/", "http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
-                .allowCredentials(true)
                 .maxAge(3600);
     }
 }
