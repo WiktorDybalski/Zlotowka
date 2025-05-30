e7f3296616029d2e848084b108770da7c2c1ae02
Kamil Rudny <k.rudny1@gmail.com>
2025-05-30 22:35:29 +0200

    darkmode

--- zlotowka-frontend/components/dashboard/charts/CustomLabel.tsx
+++ zlotowka-frontend/components/dashboard/charts/CustomLabel.tsx
@@ -2,6 +2,7 @@ import formatMoney from "@/utils/formatMoney";
 import {CustomLabelProps} from "@/interfaces/dashboard/charts/MainChart";
 
 export default function CustomLabel({ viewBox, total }: CustomLabelProps) {
+    const isDark = typeof window !== 'undefined' && document.documentElement.classList.contains('dark');
   const { cx, cy } = viewBox as { cx: number; cy: number };
 
   if (cx === undefined || cy === undefined) return null;
@@ -12,10 +13,11 @@ export default function CustomLabel({ viewBox, total }: CustomLabelProps) {
             x={cx}
             y={cy}
             className="text-lg sm:text-xl md:text-2xl font-lato"
+            fill={isDark ? '#fafafa' : '#262626'}
         >
           {formatMoney(Number(total))} PLN
         </tspan>
-        <tspan x={cx} y={cy + 24} className="text-sm">
+        <tspan x={cx} y={cy + 24} className="text-sm" fill={isDark ? '#fafafa' : '#262626'}>
           Suma
         </tspan>
       </text>
--- zlotowka-frontend/components/general/DatePicker.tsx
+++ zlotowka-frontend/components/general/DatePicker.tsx
@@ -21,11 +21,12 @@ export default function DatePicker({
   setIsOpenAction,
   setDateAction,
 }: DatePickerProps) {
+  const isDark = typeof window !== 'undefined' && document.documentElement.classList.contains('dark');
   return (
         <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="pl">
           {isOpen && (
             <DateCalendar
-              className="border-border-color border-[1px] rounded-[5px] max-w-67 lg:max-w-76 absolute z-[9999] bg-background"
+              className="border-border-color border-[1px] rounded-[5px] max-w-67 lg:max-w-76 absolute z-[9999] bg-background dark:bg-accent"
               value={dayjs(currentDate)}
               onChange={(newValue) => {
                 setDateAction(newValue);
@@ -34,6 +35,19 @@ export default function DatePicker({
               sx={{
                 width: "300",
                 overflow: "hidden",
+                color: isDark ? "#fafafa" : "#212121",
+                "& .MuiDayCalendar-weekDayLabel": {
+                  color: isDark ? "#fafafa" : "#212121",
+                },
+                "& .MuiPickersArrowSwitcher-button": {
+                  color: isDark ? "#fafafa" : "#212121",
+                },
+                "& .MuiPickersDay-root": {
+                  color: isDark ? "#fafafa" : "#212121",
+                },
+                "& .MuiPickersCalendarHeader-switchViewButton" : {
+                  color: isDark ? "#fafafa" : "#212121",
+                },
                 ".Mui-selected": {
                   backgroundColor: "#262626 !important",
                   "&:focus": {
