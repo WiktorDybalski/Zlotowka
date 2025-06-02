782114c5927d60864587233a1a22a98b55fe4e8f
Kamil Rudny <134285490+krudny@users.noreply.github.com>
2025-06-03 00:23:41 +0200

    Frequency fix (#59)

--- zlotowka-frontend/components/transactions/AddTransaction.tsx
+++ zlotowka-frontend/components/transactions/AddTransaction.tsx
@@ -16,10 +16,10 @@ export default function AddTransaction({
   const mutation = useMutation({
     mutationFn: async (data: TransactionData) => {
       const res =
-        data.frequency.code === "No period"
+        data.frequency.code === "Brak okresu"
           ? TransactionService.createNewOneTimeTransaction(data)
           : TransactionService.createNewRecurringTransaction(data);
-      const isRecurring = data.frequency.code !== "No period";
+      const isRecurring = data.frequency.code !== "Brak okresu";
       toast.promise(res as Promise<unknown>, {
         loading: `Dodawanie transakcji ${
           isRecurring ? "rekurencyjnej" : "jednorazowej"
